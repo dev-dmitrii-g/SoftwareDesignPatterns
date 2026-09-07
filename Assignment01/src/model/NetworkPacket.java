aae37eb11de797c9e671f7b1de52eca0af1aebe9
dev-dmitrii-g <dev-dmitrii-g@proton.me>
2026-09-07 23:04:58 +0500

    feat: add NetworkPacket product

---
+++ Assignment01/src/model/NetworkPacket.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.net.InetAddress;
+
+public class NetworkPacket {
+    private final InetAddress sourceIP;
+    private final InetAddress destinationIP;
+    private final int sourcePort;
+    private final int destinationPort;
+    private final Protocol protocol;
+    private final String payload;
+
+    NetworkPacket(InetAddress sourceIP, InetAddress destinationIP, int sourcePort, int destinationPort, Protocol protocol, String payload) {
+        this.sourceIP = sourceIP;
+        this.destinationIP = destinationIP;
+        this.sourcePort = sourcePort;
+        this.destinationPort = destinationPort;
+        this.protocol = protocol;
+        this.payload = payload;
+    }
+
+    public InetAddress getSourceIP() {
+        return sourceIP;
+    }
+
+    public InetAddress getDestinationIP() {
+        return destinationIP;
+    }
+
+    public int getSourcePort() {
+        return sourcePort;
+    }
+
+    public int getDestinationPort() {
+        return destinationPort;
+    }
+
+    public Protocol getProtocol() {
+        return protocol;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    @Override
+    public String toString() {
+        return "NetworkPacket{" + "sourceIP=" + sourceIP + ", destinationIP=" + destinationIP + ", sourcePort=" + sourcePort + ", destinationPort=" + destinationPort + ", protocol=" + protocol + ", payload=" + payload + '}';
+    }
+}
